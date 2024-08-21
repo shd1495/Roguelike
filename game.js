ba6139b0ef1b69452c463c5530fb45226d2937df
박지영 <124862122+shd1495@users.noreply.github.com>
2024-08-21 17:13:54 +0900

    도망 기능 / 스테이지 클리어 / 게임 종료
    도망 기능 / 스테이지 클리어 / 게임 종료

--- game.js
+++ game.js
@@ -7,8 +7,12 @@ function displayStatus(stage, player, monster) {
   console.log(chalk.magentaBright(`\n=== Current Status ===`));
   console.log(
     chalk.cyanBright(`| Stage: ${stage} `) +
-      chalk.blueBright(`| player hp = ${player.hp} damege = ${player.damege} `) +
-      chalk.redBright(`| monster hp = ${monster.hp} damege = ${monster.damege} |`),
+      chalk.blueBright(
+        `| player hp = ${player.hp} damege = ${player.minDamege} ~ ${player.maxDamege}`,
+      ) +
+      chalk.redBright(
+        `| monster hp = ${monster.hp} damege = ${monster.damege} ~ ${monster.damege + 5 + stage} |`,
+      ),
   );
   console.log(chalk.magentaBright(`=====================\n`));
 }
@@ -16,6 +20,9 @@ function displayStatus(stage, player, monster) {
 const battle = async (stage, player, monster) => {
   let logs = [];
   let turnCnt = 1;
+  let aliveMonster = 1;
+  let alivePlayer = true;
+
   // 스테이지 클리어 보상 로그
   if (stage > 1) {
     player.hp += 50;
@@ -28,15 +35,9 @@ const battle = async (stage, player, monster) => {
 
     logs.forEach((log) => console.log(log));
 
-    // 몬스터 hp가 0 이하가 되면 스테이지 클리어
-    if (monster.hp <= 0) {
-      const anykeys = readlineSync.question('스테이지 클리어! 아무키나 입력해주세요');
-      break;
-    }
-
-    console.log(chalk.green(`\n1. 공격한다 2. 아무것도 하지않는다.`));
+    console.log(chalk.green(`\n1. 공격한다 2. 도망간다.(5% 확률)`));
     // 몬스터의 공격
-    monster.attack(player);
+    const maResult = monster.attack(player, stage);
 
     // 플레이어의 선택에 따라 다음 행동 처리
     const choice = readlineSync.question('당신의 선택은? ');
@@ -45,30 +46,64 @@ const battle = async (stage, player, monster) => {
       // 1번 입력시
       case '1':
         // 공격 메소드
-        player.attack(monster);
-        logs.push(chalk.green(`[${turnCnt}] 몬스터에게 ${player.damege}의 피해를 입혔습니다.`));
-        logs.push(
-          chalk.red(`[${turnCnt}] 플레이어가 몬스터에게 ${monster.damege}의 피해를 입었습니다.`),
-        );
+        const paResult = player.attack(monster);
+        logs.push(chalk.green(`[${turnCnt}] 몬스터에게 ${paResult}의 피해를 입혔습니다.`));
+        logs.push(chalk.red(`[${turnCnt}] 플레이어가 몬스터에게 ${maResult}의 피해를 입었습니다.`));
+        if (monster.hp <= 0) logs.push(`[${turnCnt}] 몬스터를 처치 했습니다!`);
 
         // 턴 카운트
         turnCnt++;
         break;
 
       case '2':
-        logs.push(chalk.red(`[${turnCnt}] 플레이어가 아무것도 하지 않았습니다.`));
-        logs.push(
-          chalk.red(`[${turnCnt}] 플레이어가 몬스터에게 ${monster.damege}의 피해를 입었습니다.`),
-        );
-        // 턴 카운트
-        turnCnt++;
-        break;
+        if (player.run()) {
+          logs.push(chalk.green(`[${turnCnt}] 도망에 성공했습니다!`));
+          monster.hp = 0;
+          break;
+        } else {
+          logs.push(chalk.yellow(`[${turnCnt}] 도망에 실패했습니다!`));
+          logs.push(
+            chalk.red(`[${turnCnt}] 플레이어가 몬스터에게 ${maResult}의 피해를 입었습니다.`),
+          );
+          // 턴 카운트
+          turnCnt++;
+          break;
+        }
 
       default:
         logs.push(chalk.red('올바른 선택을 입력해주세요'));
         break;
     }
-    if (monster.hp <= 0) logs.push(`[${turnCnt}] 몬스터를 처치 했습니다!`);
+    // 스테이지 클리어 조건 및 게임 클리어 조건
+    if (monster.hp <= 0 || player.hp <= 0) {
+      console.clear();
+
+      displayStatus(stage, player, monster);
+
+      logs.forEach((log) => console.log(log));
+
+      //게임 클리어
+      if (stage == 10) {
+        console.clear();
+
+        displayStatus(stage, player, monster);
+
+        readlineSync.question('게임 클리어를 축하드립니다.');
+        return 0;
+      }
+
+      if (monster.hp <= 0) {
+        readlineSync.question('스테이지 클리어! 아무키나 입력해주세요');
+        // 몬스터 처치
+        aliveMonster = 0;
+        return aliveMonster;
+      } else {
+        readlineSync.question('체력을 모두 소진했습니다. GAME OVER');
+        // 플레이어 사망
+        alivePlayer = false;
+        return alivePlayer;
+      }
+    }
   }
 };
 
@@ -79,11 +114,16 @@ export async function startGame() {
 
   while (stage <= 10) {
     const monster = new Monster(stage);
-    await battle(stage, player, monster);
+    const clear = await battle(stage, player, monster);
 
     // 스테이지 클리어 및 게임 종료 조건
-    console.log(player.hp);
-
-    stage++;
+    // 게임 종료
+    if (clear === false) {
+      break;
+    }
+    // 몬스터 hp가 0 이하가 되면 스테이지 클리어
+    if (clear === 0) {
+      stage++;
+    }
   }
 }
--- player.js
+++ player.js
@@ -1,10 +1,24 @@
 export class Player {
   constructor() {
     this.hp = 100;
-    this.damege = 5;
+    this.minDamege = 1;
+    this.maxDamege = 5;
   }
 
+  // 공격
   attack(monster) {
-    monster.hp -= this.damege;
+    const result = this.minDamege + Math.floor(Math.random() * (this.maxDamege - this.minDamege));
+
+    monster.hp -= result;
+
+    return result;
+  }
+
+  // 도망
+  run() {
+    // 25% 확률로 도망
+    const run = Math.floor(Math.random() * 100);
+
+    return run < 100 ? true : false;
   }
 }
