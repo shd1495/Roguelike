35babe9a316e7045ec4e980d4404d503ac656d59
박지영 <124862122+shd1495@users.noreply.github.com>
2024-08-23 10:43:32 +0900

    로그 파일 분리 / 아이템 명 표시
    로그 파일 분리 / 아이템 명 표시

--- game.js
+++ game.js
@@ -3,42 +3,9 @@ import readlineSync from 'readline-sync';
 import { Player } from './player.js';
 import { Monster } from './monster.js';
 import { Item } from './item.js';
-
-function displayStatus(stage, player, monster) {
-  console.log(chalk.magentaBright(`\n=== Current Status ===`));
-  console.log(
-    chalk.cyanBright(`| Stage: ${stage} \n`) +
-      chalk.blueBright(
-        `| player  체력 = ${player.hp} 공격력 = ${player.damage} ~ ${Math.round(player.damage * player.maxDamageMag)}`,
-        ` 방어력 = ${player.defense} 치명타 확률 = ${player.criticalChance}`,
-        ` 최대 공격력 배율 = ${player.maxDamageMag} |`,
-      ) +
-      chalk.redBright(
-        `\n| monster 체력 = ${monster.hp} 공격력 = ${monster.damage} ~ ${Math.round(monster.damage * monster.maxDamageMag)}`,
-        ` 방어력 = ${monster.defense} 치명타 확률 = ${monster.criticalChance}`,
-        ` 최대 공격력 배율 = ${monster.maxDamageMag} |`,
-      ),
-  );
-  console.log(chalk.magentaBright(`=====================\n`));
-}
-
-// 플레이어 로그
-function handlePlayerLog(turnCnt, result, logs) {
-  logs.push(
-    chalk.green(
-      `[${turnCnt}] 몬스터에게 ${result[1] ? '치명타로' : ''} ${result[0]}의 피해를 입혔습니다!`,
-    ),
-  );
-}
-
-// 몬스터 로그
-function handleMonsterLog(turnCnt, result, logs) {
-  logs.push(
-    chalk.red(
-      `[${turnCnt}] 플레이어가 ${result[1] ? '치명타로' : ''} ${result[0]}의 피해를 입었습니다!`,
-    ),
-  );
-}
+import { displayStatus } from './loggers.js';
+import { handlePlayerLog } from './loggers.js';
+import { handleMonsterLog } from './loggers.js';
 
 const battle = async (stage, player, monster) => {
   let logs = [];
@@ -164,8 +131,9 @@ const battle = async (stage, player, monster) => {
 
       // 스테이지 클리어
       if (monster.hp <= 0 && player.hp > 0) {
+        // 아이템 드랍
         // 2 ~ 20% 확률로 아이템 드랍
-        if (Math.random() * 100 < 2 * stage) {
+        if (Math.random() * 100 < 100) {
           const item = Item.dropItem();
           console.log(chalk.yellow(`몬스터가 ${item.name}을/를 드랍했습니다!`));
           item.equipItem(player, item);
